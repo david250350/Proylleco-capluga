9eb3c09a05690bb4ba531fda5feb4c0a91b4f024
David García <tecnogarbal01@gmail.com>
2024-06-17 23:05:29 -0600

    carrito

---
+++ api/CaplugaAPI/Controllers/CarritoController.cs
@@ -0,0 +1,127 @@
+﻿using CaplugaAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CaplugaAPI.Controllers
+{
+    public class CarritoController : ApiController
+    {
+
+        [HttpPost]
+        [Route("RegistrarCarrito")]
+        public string RegistrarCarrito(Cart cart)
+        {
+            using (var context = new CAPLUGAEntities())
+            {
+                var datos = (from x in context.Cart
+                             where x.UserID == cart.CartID
+                                && x.MedicalImplementsID == cart.MedicalImplementsID
+                             select x).FirstOrDefault();
+
+                if (datos == null)
+                {
+                    context.Cart.Add(cart);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    datos.Quantity = cart.Quantity;
+                    context.SaveChanges();
+                }
+                return "OK";
+            }
+        }
+
+        [HttpGet]
+        [Route("ConsultarCarrito")]
+        public object ConsultarCarrito(long q)
+        {
+            using (var context = new CAPLUGAEntities())
+            {
+                context.Configuration.LazyLoadingEnabled = false;
+                return (from x in context.Cart
+                        join y in context.MedicalImplements on x.MedicalImplementsID equals y.MedicalImplementsID
+                        where x.UserID == q
+                        select new
+                        {
+                            x.CartID,
+                            x.UserID,
+                            x.MedicalImplementsID,
+                            x.Quantity,
+                            y.Name,
+                            y.Price,
+                            SubTotal = (y.Price * x.Quantity),
+                            Impuestos = (y.Price * x.Quantity) * 0.13M,
+                            Total = (y.Price * x.Quantity) + (y.Price * x.Quantity) * 0.13M
+                        }).ToList();
+            }
+        }
+
+        [HttpGet]
+        [Route("ConsultaCarritoadm")]
+        public List<Detail> ConsultaCarritoadm()
+        {
+            try
+            {
+
+                using (var context = new CAPLUGAEntities())
+                {
+                    context.Configuration.LazyLoadingEnabled = false;
+                    return (from x in context.Detail
+                            select x).ToList();
+                }
+            }
+            catch (Exception)
+            {
+                return new List<Detail>();
+            }
+        }
+
+        
+
+        
+
+        [HttpPost]
+        [Route("PagarCarrito")]
+        public int PagarCarrito(Cart cart)
+        {
+            using (var context = new CAPLUGAEntities())
+            {
+                return context.PayCart(cart.UserID);
+            }
+        }
+
+        [HttpDelete]
+        [Route("EliminarRegistroCarrito")]
+        public void EliminarRegistroCarrito(long q)
+        {
+            using (var context = new CAPLUGAEntities())
+            {
+                var datos = (from x in context.Cart
+                             where x.CartID == q
+                             select x).FirstOrDefault();
+
+                context.Cart.Remove(datos);
+                context.SaveChanges();
+            }
+        }
+
+        [HttpPut]
+        [Route("ActualizarEstadoPago")]
+        public string ActualizarEstadoPago(FacturaEnt entidad)
+        {
+            using (var context = new CAPLUGAEntities())
+            {
+                context.ApprovePaymentDetails(entidad.MasterPurchaseID);
+
+                return "OK";
+            }
+
+        }
+    }
+}
---
+++ web/Capluga/Models/CarritoModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Web;
+using Capluga.Entities;
+
+namespace Capluga.Models
+{
+    public class CarritoModel
+    {
+
+        public string rutaServidor = ConfigurationManager.AppSettings["RutaApi"];
+
+        public string RegistrarCarrito(CarritoEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "RegistrarCarrito";
+                var jsonData = JsonContent.Create(entidad);
+                var res = client.PostAsync(urlApi, jsonData).Result;
+                return res.Content.ReadFromJsonAsync<string>().Result;
+            }
+        }
+
+        public List<CarritoEnt> ConsultarCarrito(long q)
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "ConsultarCarrito?q=" + q;
+                var res = client.GetAsync(urlApi).Result;
+                return res.Content.ReadFromJsonAsync<List<CarritoEnt>>().Result;
+            }
+        }
+
+        public List<FacturaEnt> ConsultaCarritoadm()
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "ConsultaCarritoadm";
+                var res = client.GetAsync(urlApi).Result;
+                return res.Content.ReadFromJsonAsync<List<FacturaEnt>>().Result;
+            }
+        }
+
+
+
+
+
+        public int PagarCarrito(CarritoEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "PagarCarrito";
+                var jsonData = JsonContent.Create(entidad);
+                var res = client.PostAsync(urlApi, jsonData).Result;
+                return res.Content.ReadFromJsonAsync<int>().Result;
+            }
+        }
+
+        public void EliminarRegistroCarrito(long q)
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "EliminarRegistroCarrito?q=" + q;
+                var res = client.DeleteAsync(urlApi).Result;
+            }
+        }
+
+    }
+}
