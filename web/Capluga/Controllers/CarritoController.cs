0e2a07be87f946651cd5cf6fa300ff2faf6de6db
David García <tecnogarbal01@gmail.com>
2024-06-27 20:44:23 -0600

    carrito

---
+++ web/Capluga/Controllers/CarritoController.cs
@@ -0,0 +1,65 @@
+﻿using Capluga.Entities;
+using Capluga.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capluga.Controllers
+{
+    public class CarritoController : Controller
+    {
+        CarritoModel modelCarrito = new CarritoModel();
+
+        [HttpGet]
+        public ActionResult RegistrarCarrito(int Quantity, long MedicalImplementsID)
+        {
+            var entidad = new CarritoEnt();
+            entidad.UserID = long.Parse(Session["UserID"].ToString());
+            entidad.MedicalImplementsID = MedicalImplementsID;
+            entidad.Quantity = Quantity;
+
+            modelCarrito.RegistrarCarrito(entidad);
+
+            var datos = modelCarrito.ConsultarCarrito(long.Parse(Session["UserID"].ToString()));
+            Session["Cant"] = datos.AsEnumerable().Sum(x => x.Quantity);
+            Session["SubT"] = datos.AsEnumerable().Sum(x => x.SubTotal);
+
+            return Json("OK", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult ConsultaCarrito()
+        {
+            var datos = modelCarrito.ConsultarCarrito(long.Parse(Session["UserID"].ToString()));
+            Session["TotalPago"] = datos.AsEnumerable().Sum(x => x.Total);
+            return View(datos);
+        }
+
+
+
+
+
+        [HttpGet]
+        public ActionResult EliminarRegistroCarrito(long q)
+        {
+            modelCarrito.EliminarRegistroCarrito(q);
+
+            var datos = modelCarrito.ConsultarCarrito(long.Parse(Session["UserID"].ToString()));
+            Session["Cant"] = datos.AsEnumerable().Sum(x => x.Quantity);
+            Session["SubT"] = datos.AsEnumerable().Sum(x => x.SubTotal);
+            return RedirectToAction("ConsultaCarrito", "Carrito");
+        }
+        [HttpGet]
+        public ActionResult ConsultaCarritoadm()
+        {
+            var datos = modelCarrito.ConsultaCarritoadm();
+            return View(datos);
+        }
+
+
+
+
+    }
+}
---
+++ web/Capluga/Entities/CarritoEnt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capluga.Entities
+{
+    public class CarritoEnt
+    {
+        public long CartID { get; set; }
+        public long UserID { get; set; }
+        public long MedicalImplementsID { get; set; }
+        public int Quantity { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Impuestos { get; set; }
+        public decimal Total { get; set; }
+    }
+}
